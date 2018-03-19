9ca05efe5874866242a43ddd6ac029d3468aebc4
USER <USER@INET-10>
2018-03-19 16:47:34 +0900

    commit

---
+++ Chapter_04/src/Student.java
@@ -0,0 +1,18 @@
+
+public class Student {
+   private int number;
+   private String name;
+   private int age;
+   
+   Student(){
+	   number = 100;
+	   name = "new Student";
+	   age = 18;
+			   
+   }
+   Student(int unmber, String name, int age){
+	   this.number = number;
+	   this.name = name;
+	   this.age = age;
+   }
+}
