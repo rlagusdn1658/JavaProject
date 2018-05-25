c5bfefde4e89ed98e067f910749d32e3dc19bd04
USER <USER@PRO-18>
2018-05-25 11:45:49 +0900

    commit

---
+++ Chapter_10/src/HandPhone.java
@@ -0,0 +1,64 @@
+
+
+public class HandPhone {
+static String fname = "C:\\temp\\iuso.txt";
+
+
+static class address{
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	String name;
+	String age;
+	String phone;
+	
+	address(String name, String age,String phone){
+		this.name = name;
+		this.age = age;
+		this.phone = phone;
+		
+	}
+}
+	
+	public static void main(String[] args){
+    
+	}
+	static void print_menu(){
+		
+	}
+	static void view_juso(){
+
+	}
+	 static void add_juso(){
+		
+	}
+	static void delete_juso(){
+		
+	}
+}
+
+
+      
+	
+	
