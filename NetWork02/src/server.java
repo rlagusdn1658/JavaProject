23eea19a35c295bfad9b0f922ca01d21755d15a4
USER <USER@INET-06>
2018-06-22 11:47:16 +0900

    commit

--- NetWork02/src/server.java
+++ NetWork02/src/server.java
@@ -13,135 +13,119 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-class FrServer extends JFrame{
-JTextArea ta;
-JTextField tf;
 
+class FrServer extends JFrame {
+	JTextArea ta;
+	JTextField tf;
 
-public FrServer(){
-	 setSize(600,400);
-	 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	 setTitle("채팅방서버");
-	 setLayout(new BorderLayout());
-	 setVisible(true);
-	 
-	 JLabel label = new JLabel("This is a server !");
-	 ta = new JTextArea(25, 40);
-	 tf = new JTextField(25);
-	 
-	 add(label, BorderLayout.NORTH);
-	 add(ta, BorderLayout.CENTER);
-	 add(tf, BorderLayout.SOUTH);
-	 
+	public FrServer() {
+		setSize(600, 400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("채팅방서버");
+		setLayout(new BorderLayout());
+		
+
+		JLabel label = new JLabel("This is a server !");
+		ta = new JTextArea(25, 40);
+		tf = new JTextField(25);
+
+		add(label, BorderLayout.NORTH);
+		add(ta, BorderLayout.CENTER);
+		add(tf, BorderLayout.SOUTH);
+		setVisible(true);
 	}
 }
 
+public class server {
+	HashMap clients;
 
+	server() {
+		clients = new HashMap();
+		Collections.synchronizedMap(clients);
+	}
 
-     public class server{
-    	 HashMap clients;
-    	 
-    	 server(){
-    		 clients = new HashMap();
-    		 Collections.synchronizedMap(clients);
-    	 }
-    	 
-    	 
-   
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		FrServer f = new FrServer();
 		new server().start();
 
 	}
-	public void start(){
+
+	public void start() {
 		ServerSocket serverSocket = null;
 		Socket socket = null;
-		try{
+		try {
 			serverSocket = new ServerSocket(7777);
 			System.out.println("서버가 시작되었습니다");
-			
-			while(true){
-				socket=serverSocket.accept();
-				System.out.println("["+socket.getInetAddress()+"]"+"에서 접속하셨습니다");
+
+			while (true) {
+				socket = serverSocket.accept();
+				System.out.println("[" + socket.getInetAddress() + "]" + "에서 접속하셨습니다");
 				ServerReceiver thread = new ServerReceiver(socket);
 				thread.start();
-				
+
 				System.out.println("쓰레드 네임 :" + thread.getName());
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
-	
-     }catch(Exception e){
-    	 e.printStackTrace();
-     }
-}
-	
-	void sendToAll(String msg){
+	}
+
+	void sendToAll(String msg) {
 		Iterator it = clients.keySet().iterator();
-		
-		while(it.hasNext()){
-			try{
-				DataOutputStream out =(DataOutputStream)clients.get(it.next());
+
+		while (it.hasNext()) {
+			try {
+				DataOutputStream out = (DataOutputStream) clients.get(it.next());
 				out.writeUTF(msg);
-			}catch(IOException e){
+			} catch (IOException e) {
 				System.out.println("sendToall 입출력 에러");
 			}
 		}
 	}
-	
-	
-	class ServerReceiver extends Thread{
+
+	class ServerReceiver extends Thread {
 		Socket socket;
 		DataInputStream in;
 		DataOutputStream out;
-		
-		ServerReceiver(Socket socket){
+
+		ServerReceiver(Socket socket) {
 			this.socket = socket;
-			try{
+			try {
 				in = new DataInputStream(socket.getInputStream());
 				out = new DataOutputStream(socket.getOutputStream());
-				
-			}catch(IOException e){
+
+			} catch (IOException e) {
 				System.out.println("서버 리시버 소켓 IO 에러");
 			}
 		}
-		
-		public void run(){
-			String name="";
-			
-			try{
+
+		public void run() {
+			String name = "";
+
+			try {
 				name = in.readUTF();
-				sendToAll("#"+name+"님이 들어오셨숩니다");
+				sendToAll("#" + name + "님이 들어오셨숩니다");
 				clients.put(name, out);
-				System.out.println("현재 접속자 수는 "+clients.size()+"입니다");
+				System.out.println("현재 접속자 수는 " + clients.size() + "입니다");
 				System.out.println("현재 접속자 목록:");
-				
-				while (in!= null){
+
+				while (in != null) {
 					sendToAll(in.readUTF());
 				}
-				
-			}catch(IOException e){
+
+			} catch (IOException e) {
 				System.out.println("리시버 도중 io에러");
-			}finally{
-				sendToAll("#"+name+"님이 나가셨습니다~");
+			} finally {
+				sendToAll("#" + name + "님이 나가셨습니다~");
 				clients.remove(name);
-				System.out.println("[" + socket.getInetAddress()+":"+socket.getPort()+"] 에서 접속을 종료합니다");
-				System.out.println("현재 접속자 수는 "+clients.size()+"입니다");
+				System.out.println("[" + socket.getInetAddress() + ":" + socket.getPort() + "] 에서 접속을 종료합니다");
+				System.out.println("현재 접속자 수는 " + clients.size() + "입니다");
 			}
-			
-			
-			
+
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		}
-		
+
 	}
+
+}
--- NetWork02/src/serverClient.java
+++ NetWork02/src/serverClient.java
@@ -1,9 +1,15 @@
+import java.io.IOException;
 
 public class serverClient {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	     
+	       
+	    
+	    public static void main(String[] args) throws IOException {
+	    	       
+	    	 
+	    }
 
 	}
 
-}
+
